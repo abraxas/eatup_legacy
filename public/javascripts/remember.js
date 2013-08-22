0932e32e0286e1fe529416e8b25ad12482f69cec
C.Jay <cjay.martin@gmail.com>
2013-08-22 12:01:38 -0400

    Be awesome to extrapolate this... server-side state can be transmitted magically to client

---
+++ public/javascripts/remember.js
@@ -0,0 +1,34 @@
+var foo = function() { console.log("FOO"); }
+
+var remember = function() {
+  var remember_data = {};
+
+  var me = function(key,value) {
+    //    console.log(JSON.stringify(remember_data));
+    if(typeof window !== 'undefined') {
+      if(!remember.loaded) {
+        remember_data = $('body').data('remember') || {};
+        remember.loaded = 1;
+      }
+    }
+    //node
+
+    if(value) { remember_data[key] = value; }
+    return remember_data[key];
+  }
+
+  me.loaded = null;
+  me.get = function() {
+    return JSON.stringify(remember_data);
+  }
+  me.forget = function() {
+    remember_data = {};
+  }
+  return me;
+}()
+
+if(typeof window === 'undefined') {
+  (function(exports) {
+    module.exports = remember
+  })(typeof exports === 'undefined'? this['mymodule']={} : exports)
+}
